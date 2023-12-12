e3531b2094c0b42a62bd26a0fe111944c8513d6c
tatfil <66638120+tatfil@users.noreply.github.com>
2023-12-12 20:47:05 +0100

    added grid and function refactoring

--- example.js
+++ example.js
@@ -4,57 +4,38 @@ const add_params = {
   per_page: 10,
 };
 
-const url = new URL("https://api.coingecko.com/api/v3/coins/markets?");
-const urlParams = new URL(url + new URLSearchParams(add_params).toString());
-console.log(urlParams);
+const urlbasic = new URL("https://api.coingecko.com/api/v3/coins/markets?");
+const urlParams = new URL(
+  urlbasic + new URLSearchParams(add_params).toString()
+);
+const urlCoins = "https://api.coingecko.com/api/v3/coins/";
 
-function fetchCoinsList() {
-  fetch(urlParams)
-    .then((response) => {
-      console.log(response);
-      if (!response.ok) {
-        throw Error("Error");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      const html = data
-        .map((currency) => {
-          return `<div id=${currency.name}  class="currency-list">
-        <p><img src="${currency.image}" alt="" </p>
-        <p>Name:  ${currency.name}</p>
-        <p>Symbol: ${currency.symbol}</p>
-        <p>Current price: ${currency.current_price}</p>
-        <p>High 24 hour Price: ${currency.high_24h}</p>
-        <p>Low 24 hour Price: ${currency.low_24h}</p>
-        <button class="currency-button" onclick="fetchCoinsById('${currency.id}')">more details</button>        
-        </div>`;
-        })
-        .join("");
-      console.log(html);
-      document
-        .querySelector("#coin-container")
-        .insertAdjacentHTML("afterbegin", html);
-    });
+function fetchCoinsList(urlParams) {
+  sendAPIRequest(urlParams).then((data) => {
+    insertCoinList(data);
+  });
 }
 
-fetchCoinsList();
+fetchCoinsList(urlParams);
+
+async function sendAPIRequest(urlParams) {
+  const response = await fetch(urlParams);
+  console.log(response);
+  if (!response.ok) {
+    throw Error("Error");
+  }
+  return await response.json();
+}
 
 function fetchCoinsById(idName) {
-  fetch(new URL("https://api.coingecko.com/api/v3/coins/" + idName).toString())
-    .then((response) => {
-      console.log(response);
-      if (!response.ok) {
-        throw Error("Error");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log("details: ");
-      console.log(data);
+  const url = new URL(new URL(urlCoins + idName).toString());
+  sendAPIRequest(url).then((data) => {
+    insertCoinDetails(data, idName);
+  });
+}
 
-      const html = `<p>Name:  ${data.name}</p>
+function insertCoinDetails(data, idName) {
+  const html = `<p>Name:  ${data.name}</p>
       <p>Symbol: ${data.symbol}</p>
       <p>Hashing algorithm: ${data.hashing_algorithm}</p>
       <p>Description: ${data.description.en}</p> 
@@ -63,10 +44,48 @@ function fetchCoinsById(idName) {
       <p>Genesis Date: ${data.genesis_date}</p>   
       `;
 
-      const idLowerCase = idName[0].toUpperCase() + idName.slice(1);
-      console.log(idLowerCase);
-      document
-        .querySelector("#" + idLowerCase)
-        .insertAdjacentHTML("beforeend", html);
-    });
+  const idLowerCase = idName[0].toUpperCase() + idName.slice(1);
+  console.log(idLowerCase);
+  document
+    .querySelector("#" + idLowerCase)
+    .insertAdjacentHTML("beforeend", html);
 }
+
+function insertCoinList(data) {
+  console.log;
+  const html = data
+    .map((currency) => {
+      return `<div id=${currency.name}  class="currency-list">
+  <p><img src="${currency.image}" alt="" </p>
+  <p>Name:  ${currency.name}</p>
+  <p>Symbol: ${currency.symbol}</p>
+  <p>Current price: ${currency.current_price}</p>
+  <p>High 24 hour Price: ${currency.high_24h}</p>
+  <p>Low 24 hour Price: ${currency.low_24h}</p>
+  <button class="currency-button" onclick="fetchCoinsById('${currency.id}')">more details</button>        
+  </div>`;
+    })
+    .join("");
+  document
+    .querySelector("#coin-container")
+    .insertAdjacentHTML("afterbegin", html);
+}
+
+function fetchExample() {
+  const user = fetch("https://jsonplaceholder.typicode.com/users/1")
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
+
+  console.log(user);
+  const printAddress = () => {
+    user.then((a) => {
+      console.log(a);
+    });
+  };
+
+  printAddress();
+}
+
+//fetchExample();
